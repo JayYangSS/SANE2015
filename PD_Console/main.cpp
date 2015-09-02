69a6848f8f9e1d5d577ca901ab5b30e10f93491a
hrkim <blastak@naver.com>
2015-09-03 00:13:52 +0900

    Ground Constraint 적용. false reduction.

--- PD_Console/main.cpp
+++ PD_Console/main.cpp
@@ -2,9 +2,12 @@
 
 int main()
 {
-	VideoCapture vcap("../CVLAB_dataset/data/$$2015-03-18-09h-59m-40s_F_normal.mp4");
+	VideoCapture vcap("../CVLAB_dataset/data/$$2015-03-18-09h-59m-40s_F_normal.mp4"); // y=0.7513x+337.75
+	//VideoCapture vcap("../CVLAB_dataset/data/2015-02-24-17h-52m-23s_F_event.avi"); // y=0.9037x+359.41
 	if (!vcap.isOpened())
 		return -1;
+
+	Rect rectROI = Rect(1280 / 4, 720 / 4, 1280 / 2, 720 / 2);
 
 	CPedestrianDetection objPD;
 	objPD.LoadClassifier("acf_classifier.txt");
@@ -20,7 +23,6 @@ int main()
 		if (imgInput.empty()) break;
 
 		Mat imgInputResz;
-		Rect rectROI = Rect(1280 / 4, 720 / 4, 1280 / 2, 720 / 2);
 		imgInputResz = imgInput(rectROI).clone();
 		imgDisp = imgInputResz.clone();
 
@@ -31,7 +33,25 @@ int main()
 		t = ((double)getTickCount() - t) / getTickFrequency();
 		printf("%.2lf ms  %.1lf fps\n", t * 1000, 1 / t);
 
-		objPD.DrawBoundingBox(imgDisp, CV_RGB(0, 0, 255));
+		vector<Rect_<int> > bb = objPD.m_vecrectDetectedBB;
+		for (int i = 0; i < (int)bb.size(); i++)
+		{
+			bb[i].x += rectROI.x;
+			bb[i].y += rectROI.y;
+		}
+
+		for (int i = 0; i < (int)bb.size(); i++)
+		{
+			double est = (double)bb[i].height*0.7513 + 337.75;
+			if (abs((double)bb[i].br().y - est) > 10)
+			{
+				objPD.m_vecrectDetectedBB.erase(objPD.m_vecrectDetectedBB.begin() + i);
+				bb.erase(bb.begin() + i);
+				i--;
+			}
+		}
+
+		objPD.DrawBoundingBox(imgDisp, CV_RGB(255, 255, 255));
 
 		imshow("Display", imgDisp);
 		cntFrames++;
